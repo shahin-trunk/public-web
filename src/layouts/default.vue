bd3fa96f8732bbd7009bbf30c3075ea8ed91a0eb
shahin-trunk <shahin.konadath@outlook.com>
2024-03-01 19:30:48 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/layouts/default.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Footer from "~/modules/layout/Footer.vue";
+import Main from "~/modules/layout/Main.vue";
+import HeaderNavbar from "../modules/navigation/HeaderNavbar.vue";
+import MobileNavigation from "../modules/navigation/MobileNavigation.vue";
+import { useScrollRestoration } from "~/modules/a11y/useScrollRestoration";
+import SkipNavigationLink from "../modules/a11y/SkipNavigationLink.vue";
+import PageMetadata from "../modules/seo/PageMetadata.vue";
+
+useScrollRestoration();
+</script>
+
+<template>
+	<PageMetadata>
+		<SkipNavigationLink />
+		<MobileNavigation />
+		<HeaderNavbar />
+		<Main>
+			<slot />
+		</Main>
+		<Footer />
+	</PageMetadata>
+</template>
